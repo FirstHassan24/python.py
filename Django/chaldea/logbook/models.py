d92f8d51c8dbfae0aa98bdc90eb96e3aedae7ce3
Akilou24 <61370309+FirstHassan24@users.noreply.github.com>
2025-10-21 17:31:51 -0400

    add:  set up for my servant database(url,views,models)

---
+++ Django/chaldea/logbook/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+class Servant(models.Model):
+    #every servants name must be unique:
+    name = models.CharField(max_length = 100, unique=True)
+    #get the class names
+    class_name = models.CharField(max_length=50,null=True,blank=True)
+    #get the rarity of the servant:
+    rarity = models.IntegerField(null=True, blank=True)
+    #get the np name of the servant:
+    np_name = models.CharField(max_ength=100,null=True, blank=True)
+   
+    #make django show the name of the servant when printing:
+    def __str__(self):
+        return self.name
---
+++ Django/chaldea/logbook/urls.py
@@ -0,0 +1,6 @@
+#import the path so we can start linking other files to url:
+from django.urls import path
+
+urlpatterns = [
+    
+]
